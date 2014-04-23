4f59ec01626cc44ef1f1c50be49f47a6c30483ea
Danny <ddorfel@netvlies.nl>
2014-04-23 21:04:13 +0200

    Added license file and php file header comments

--- Pagerfanta.php
+++ Pagerfanta.php
@@ -1,6 +1,11 @@
 <?php
-/**
- * Created 16-02-14 00:20
+/*
+ * This file is part of the Swoopaholic Framework Bundle.
+ *
+ * (c) Danny Dörfel <danny@swoopaholic.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 namespace Swoopaholic\Bridge\Pagerfanta;
 
--- View.php
+++ View.php
@@ -1,6 +1,11 @@
 <?php
-/**
- * Created 01-02-14 17:16
+/*
+ * This file is part of the Swoopaholic Framework Bundle.
+ *
+ * (c) Danny Dörfel <danny@swoopaholic.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 namespace Swoopaholic\Bridge\Pagerfanta;
 
